9ca5e485836769acd9b7a2d244b67a3226e278be
ssong24 <songeun.kim@3i.ai>
2022-01-07 10:21:15 +0900

    Initial Commit

---
+++ CppPractive/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppPractive
+//
+//  Created by Song on 2022/01/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
